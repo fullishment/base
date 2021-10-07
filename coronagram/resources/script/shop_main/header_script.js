491d2e03d1293a7908c7f79d3062c239acfb868c
edward8778 <harryp9270@gmail.com>
2021-10-07 17:05:38 +0900

    add coronagram_main

--- coronagram/resources/script/shop_main/header_script.js
+++ coronagram/resources/script/shop_main/header_script.js
@@ -13,14 +13,35 @@
     bannerTimer = setInterval(bannerSwitcher, 5000)
   });
 
+  $(document).ready(function(){
+    $(".heart").on("click",function(){
 
-///// Anchor Smooth Scroll /////
-//   $('.main-menu a, .learn-more-button a').click(function(e){
-    
-//     e.preventDefault();
-        
-//     var target = $(this).attr('href');
-        
-//     $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
-//     return false;
-//   });
+    });
+    $("#img_ch1").on("mouseover",function(){
+      $("#story_img").attr("src","../../resources/images/shop1.jpg");
+      $(".story_img_change1").css('color','black');
+      $(".story_img_change1").css('border-top','2px solid black');
+      $(".story_img_change2").css('color','rgba(80,80,80,0.7)');
+      $(".story_img_change2").css('border-top','2px solid rgba(80,80,80,0.7)');
+      $(".story_img_change3").css('color','rgba(80,80,80,0.7)');
+      $(".story_img_change3").css('border-top','2px solid rgba(80,80,80,0.7)');
+    });
+    $("#img_ch2").on("mouseover",function(){
+      $("#story_img").attr("src","../../resources/images/shop3.jpg");
+      $(".story_img_change2").css('color','black');
+      $(".story_img_change2").css('border-top','2px solid black');
+      $(".story_img_change1").css('color','rgba(80,80,80,0.7)');
+      $(".story_img_change1").css('border-top','2px solid rgba(80,80,80,0.7)');
+      $(".story_img_change3").css('color','rgba(80,80,80,0.7)');
+      $(".story_img_change3").css('border-top','2px solid rgba(80,80,80,0.7)');
+    });
+    $("#img_ch3").on("mouseover",function(){
+      $("#story_img").attr("src","../../resources/images/shop4.jpg");
+      $(".story_img_change3").css('color','black');
+      $(".story_img_change3").css('border-top','2px solid black');
+      $(".story_img_change1").css('color','rgba(80,80,80,0.7)');
+      $(".story_img_change1").css('border-top','2px solid rgba(80,80,80,0.7)');
+      $(".story_img_change2").css('color','rgba(80,80,80,0.7)');
+      $(".story_img_change2").css('border-top','2px solid rgba(80,80,80,0.7)');
+    });
+  });
