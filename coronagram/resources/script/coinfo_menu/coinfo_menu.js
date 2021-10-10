2403cf84dd9aa16167ae0abec63359a5565b34ff
edward8778 <harryp9270@gmail.com>
2021-10-10 19:46:27 +0900

    add coinfo

---
+++ coronagram/resources/script/coinfo_menu/coinfo_menu.js
@@ -0,0 +1,32 @@
+// menu  
+  
+  const toggler = document.querySelector('.menu__toggler');
+        const menu = document.querySelector('.menu');
+
+        toggler.addEventListener('click', () => {
+            toggler.classList.toggle('active');
+            menu.classList.toggle('active');
+        });
+        // 드롭다운
+        document.querySelector('.dropbtn').addEventListener('click', () => {
+            document.querySelector('.dropdown').classList.toggle('show');
+        })
+        //메뉴 화살표
+        var arrow = '<span class="arrowspan"></span>'
+        $(arrow).insertBefore('.mcon');
+
+        $('.menu .arrowspan').click(function () {
+            if (
+                $(this).next('.mcon').hasClass('deployed')) {
+                $(this).removeClass('up-arrow');
+                $(this).next('.mcon').slideUp('fast').removeClass('deployed');
+            } else {
+                $(this).addClass('up-arrow');
+                // $('.mcon').slideUp('fast').removeClass('deployed'); // Close Other Elements
+                $(this).next('.mcon').slideDown('fast').addClass('deployed');
+            }
+        });
+
+
+
+// main
