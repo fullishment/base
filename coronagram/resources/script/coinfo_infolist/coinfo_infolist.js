dd4fa7c834e8a8637ae0443b7a7a76ff733d4a48
이혜주 <haejulee.hjl@gmail.com>
2021-10-14 02:02:39 +0900

    changed coinfo

--- coronagram/resources/script/coinfo_infolist/coinfo_infolist.js
+++ coronagram/resources/script/coinfo_infolist/coinfo_infolist.js
@@ -1,30 +1,30 @@
   /* menu */
   
   
-  const toggler = document.querySelector('.menu__toggler');
-        const menu = document.querySelector('.menu');
+  const toggler = document.querySelector('.cm_menu__toggler');
+        const menu = document.querySelector('.cm_menu');
 
         toggler.addEventListener('click', () => {
             toggler.classList.toggle('active');
             menu.classList.toggle('active');
         });
         // 드롭다운
-        document.querySelector('.dropbtn').addEventListener('click', () => {
-            document.querySelector('.dropdown').classList.toggle('show');
+        document.querySelector('.cm_dropbtn').addEventListener('click', () => {
+            document.querySelector('.cm_dropdown').classList.toggle('cm_show');
         })
         //메뉴 화살표
-        var arrow = '<span class="arrowspan"></span>'
-        $(arrow).insertBefore('.mcon');
+        var cm_arrow = '<span class="cm_arrowspan"></span>'
+        $(cm_arrow).insertBefore('.cm_mcon');
 
-        $('.menu .arrowspan').click(function () {
+        $('.cm_menu .cm_arrowspan').click(function () {
             if (
-                $(this).next('.mcon').hasClass('deployed')) {
-                $(this).removeClass('up-arrow');
-                $(this).next('.mcon').slideUp('fast').removeClass('deployed');
+                $(this).next('.cm_mcon').hasClass('deployed')) {
+                $(this).removeClass('cm_up-arrow');
+                $(this).next('.cm_mcon').slideUp('fast').removeClass('deployed');
             } else {
-                $(this).addClass('up-arrow');
+                $(this).addClass('cm_up-arrow');
                 // $('.mcon').slideUp('fast').removeClass('deployed'); // Close Other Elements
-                $(this).next('.mcon').slideDown('fast').addClass('deployed');
+                $(this).next('.cm_mcon').slideDown('fast').addClass('deployed');
             }
         });
 
